5b1d704fc248d6d4859c21b8d315fbec71d4d20c
willyanne11 <willyannemayara5@gmail.com>
2024-12-02 19:50:50 -0300

    scripts.js

---
+++ scripts.js
@@ -0,0 +1,11 @@
+let tempC = parseFloat(prompt('Digite a sua temperatura em °C: '));
+
+let conversao = (tempC * 9/5) + 32;
+let tempF = conversao;
+
+alert('A sua temperatura de ' + tempC + '°C' + ' para Fahrenheit é de: ' + tempF + 'F');
+
+
+
+
+
